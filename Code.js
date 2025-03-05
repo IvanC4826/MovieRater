3c49eef31c1e7a3a789a0287ce53e4a4f76ab1a8
IvanC4826 <ichiu4826@gmail.com>
2025-03-05 18:07:54 -0500

    implement start()

--- Code.js
+++ Code.js
@@ -1,22 +1,38 @@
-onload 
+function start(){
+    var row = [
+        ["https://m.media-amazon.com/images/M/MV5BNThiZjA3MjItZGY5Ni00ZmJhLWEwN2EtOTBlYTA4Y2E0M2ZmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", "Spider-Man: Across the Spider-Verse", "Super Hero", "2h 20m"]
 
+        //["imgURL", "title", "genre", "duration"]
+    ];
 
+    for (let i = 0; row.length > i; ++i){
+        addToTable((row[i][1]), (row[i][2]), (row[i][3]), (row[i][4]));
+    }
+}
 
-
-
-
-
-
-
-
-
-function addToTable() {
+function addToTable(imgURL, title, genre, duration) {
     let newRow = document.getElementById("newRow");
     newRow.innerHTML+= `<tr>
-                            <th scope="row">1</th>
-                            <td></td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
+
+                            <th scope="row">
+                                <div class="text-center">
+                                    <img src="`+imgURL+`" class="rounded rowImg" alt="Image">
+                                </div>
+                            </th>
+                            <td>`+title+`</td>
+                            <td>`+genre+`</td>
+                            <td>`+duration+`</td>
+                            <td>
+                                <select class="custom-select" id="inputGroupSelect01">\
+                                    <option value="1" selected>Not Watched</option>
+                                    <option value="2">In Progress</option>
+                                    <option value="3">Watched</option>
+                                </select>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-danger" onclick="remove(this)">Delete</button>
+                            </td>
+
                         </tr>`
     
 }
