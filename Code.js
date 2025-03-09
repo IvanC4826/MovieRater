0c964b890590679b64b8660ba03bbb20cd9d7341
IvanC4826 <ichiu4826@gmail.com>
2025-03-08 20:30:50 -0500

    Finalize

--- Code.js
+++ Code.js
@@ -93,41 +93,33 @@ function addToTable(imgURL, title, genre, date) {
 }
 
 function sort(selected) {//W3School how to
-
-    var radio = document.getElementsByName("sortRadio");
     
-    var table, switching, i, x, y, shouldSwitch;
     var rows = document.getElementById("movieTable").getElementsByTagName("tr");
 
-    table = document.getElementById("movieTable");
-    switching = true;
-    /*Make a loop that will continue until
-    no switching has been done:*/
+    var switching = true;
+
     while (switching) {
-        //start by saying: no switching is done:
+
         switching = false;
-        /*Loop through all table rows (except the
-        first, which contains table headers):*/
-        for (i = 1; i < (rows.length - 1); i++) {
-            //start by saying there should be no switching:
-            shouldSwitch = false;
-            /*Get the two elements you want to compare,
-            one from current row and one from the next:*/
-            x = rows[i].getElementsByTagName("td")[selected];
-            y = rows[i + 1].getElementsByTagName("td")[selected];
-            //check if the two rows should switch place:
+
+        for (var i = 1; i < (rows.length - 1); i++) {
+
+            var shouldSwitch = false;
+            var x = rows[i].getElementsByTagName("td")[selected];
+            var y = rows[i + 1].getElementsByTagName("td")[selected];
+
             if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                //if so, mark as a switch and break the loop:
                 shouldSwitch = true;
                 break;
+
             }
         }
         if (shouldSwitch) {
-        /*If a switch has been marked, make the switch
-        and mark that a switch has been done:*/
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+
         }
+
     }
 
 }
